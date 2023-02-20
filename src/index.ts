f9a20e4c2e718be08a1210cdfe8d5d66f7e87ea5
Fabio Coelho <fccoelho7@gmail.com>
2023-02-20 12:20:12 -0300

    create new entry file

---
+++ src/index.ts
@@ -0,0 +1,24 @@
+// Core
+export { default as Alert } from "./components/Alert";
+export { default as Avatar } from "./components/Avatar";
+export { default as Badge } from "./components/Badge";
+export { default as Button } from "./components/Button";
+export { default as ButtonGroup } from "./components/ButtonGroup";
+export { default as Modal } from "./components/Modal";
+export { default as Pagination } from "./components/Pagination";
+export { default as Tab } from "./components/Tab";
+export { default as Table } from "./components/Table";
+
+// Forms
+export { default as Checkbox } from "./components/forms/Checkbox";
+export { default as TextInput } from "./components/forms/TextInput";
+export { default as Radio } from "./components/forms/Radio";
+export { default as Select } from "./components/forms/Select";
+export { default as Textarea } from "./components/forms/Textarea";
+export { default as Toggle } from "./components/forms/Toggle";
+export { default as Label } from "./components/forms/Label";
+export { default as Error } from "./components/forms/Error";
+export { default as Helper } from "./components/forms/Helper";
+
+// Theme
+export { default as theme } from "./theme";
