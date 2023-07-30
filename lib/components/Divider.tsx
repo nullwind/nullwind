3c75604e8faad880a5c419be034ac0f9abefa2e8
Fabio Coelho <fccoelho7@gmail.com>
2023-07-30 18:22:47 -0300

    refactor: divider component adding more customizations (#33)

--- lib/components/Divider.tsx
+++ lib/components/Divider.tsx
@@ -4,17 +4,56 @@ import tc from "../tc";
 import type { BaseProps } from "../types";
 
 export const baseDivider = {
-  base: "my-8 text-sm flex items-center gap-4 before:h-px before:flex-1 before:bg-slate-300 before:content-[''] after:h-px after:flex-1 after:bg-slate-300 after:content-[''] text-slate-500",
+  base: "border-t border-gray-200 w-full",
+  variants: {
+    withLabel: {
+      true: "border-t-0",
+    },
+  },
+  slots: {
+    label: {
+      base: "text-sm text-gray-500 flex items-center before:bg-gray-200 after:bg-gray-200 before:h-px before:flex-1 after:h-px after:flex-1 gap-2",
+      variants: {
+        position: {
+          left: "before:hidden",
+          center: "",
+          right: "after:hidden",
+        },
+      },
+    },
+  },
 };
 
 interface DividerProps extends BaseProps {
   label?: string;
+  labelPosition?: "left" | "center" | "right";
+  labelProps?: any;
 }
 
-function Divider({ class: klass, label = "More", theme }: NullstackClientContext<DividerProps>) {
+function Divider({
+  class: klass,
+  label,
+  labelPosition,
+  labelProps,
+  theme,
+}: NullstackClientContext<DividerProps>) {
   const divider = tc(baseDivider, theme?.divider);
+  const { base, label: labelBase } = divider();
+  const withLabel = !!label;
 
-  return <div class={divider({ class: klass })}>{label}</div>;
+  return (
+    <div class={base({ withLabel, class: klass })}>
+      {withLabel && (
+        <element
+          tag="span"
+          {...labelProps}
+          class={labelBase({ position: labelPosition, class: labelProps?.class })}
+        >
+          {label}
+        </element>
+      )}
+    </div>
+  );
 }
 
 export default Divider as NullstackFunctionalComponent<DividerProps>;
